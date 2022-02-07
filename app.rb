fdbb1b4d511975e9bf946c1df874ef32a273aa15
Peter O <harmless_goose@icloud.com>
2022-02-07 16:50:22 +0000

    9. Sinatra Views

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader' if development? 
-
+# routing file 
 get '/' do
   "hello!"
 end
@@ -9,8 +9,6 @@ get '/secret' do
     "Makers is lots of fun!"
 end
 
-get '/cat' do
-    "<div style='border: 3px dashed red'> 
-    <img src='https://i.imgur.com/jFaSxym.png'>
-    </div>"
+get '/cat' do 
+    erb(:index) # method from templating system that renders file called index.erb
 end 
